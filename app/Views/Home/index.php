bbb2e88feddaa85f7fa6e54928bd7551d8501e51
Miguel <miguel@bwired.ca>
2021-06-08 17:36:03 -0400

    seccion 6 registro de usuarios y validacion de campos

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -3,5 +3,5 @@
 <?= $this->section("content") ?>
 
     <h1>WELCOME</h1>
-
+    <a href="<?= site_url('/signup') ?>">Sign up</a>
 <?= $this->endSection() ?>
---
+++ app/Views/Signup/success.php
@@ -0,0 +1,6 @@
+<?= $this->extend('layouts/default') ?>
+<?= $this->section('title') ?>Success<?= $this->endSection() ?>
+<?= $this->section('content') ?>
+<h1>Success register</h1>
+    <p>signup successful</p>
+<?= $this->endSection() ?>
